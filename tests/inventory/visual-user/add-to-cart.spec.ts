a5729a17448cf094652594f50bb7915eed6d5e08
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-09-27 20:45:52 -0300

    created te inventory page for visual_user

---
+++ tests/inventory/visual-user/add-to-cart.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../../pages/LoginPage";
+import { InventoryPage } from "../../../pages/InventoryPage";
+import { CartPage } from "../../../pages/CartPage";
+
+const productList = [
+  "Sauce Labs Backpack",
+  "Sauce Labs Bike Light",
+  "Sauce Labs Bolt T-Shirt",
+  "Sauce Labs Fleece Jacket",
+  "Sauce Labs Onesie",
+  "Test.allTheThings() T-Shirt (Red)",
+];
+
+test.describe("Add to cart visual_user tests", () => {
+  let loginPage: LoginPage;
+  let inventoryPage: InventoryPage;
+  let cartPage: CartPage;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    inventoryPage = new InventoryPage(page);
+    cartPage = new CartPage(page);
+
+    await loginPage.goto();
+    await loginPage.login("visual_user", "secret_sauce");
+  });
+
+  productList.forEach((product) => {
+    test(`Should add ${product} and verify in cart page`, async () => {
+      await inventoryPage.addItemToCartByName(product);
+
+      const itemCount = await inventoryPage.getCartItemCount();
+      expect(itemCount).toBe(1);
+
+      await cartPage.goto();
+
+      const cartItems = await cartPage.getCartItems();
+      expect(cartItems).toContain(product);
+    });
+  });
+});
