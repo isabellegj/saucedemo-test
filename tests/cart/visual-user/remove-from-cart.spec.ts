cd49394a54906a9a7bf55f7820a14b12bc4cb731
Isabelle G. Jesuino <isabellegjesuino@gmail.com>
2024-09-28 13:42:50 -0300

    created remove-from-cart test from cart page for each user

---
+++ tests/cart/visual-user/remove-from-cart.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../../pages/LoginPage";
+import { InventoryPage } from "../../../pages/InventoryPage";
+import { CartPage } from "../../../pages/CartPage";
+
+const productList = [
+  "Sauce Labs Backpack",
+  "Sauce Labs Bike Light",
+  "Sauce Labs Bolt T-Shirt",
+  "Sauce Labs Fleece Jacket",
+  "Sauce Labs Onesie",
+  "Test.allTheThings() T-Shirt (Red)",
+];
+
+productList.forEach((product) => {
+  test.describe("Remove from cart visual_user tests", () => {
+    let loginPage: LoginPage;
+    let inventoryPage: InventoryPage;
+    let cartPage: CartPage;
+
+    test.beforeEach(async ({ page }) => {
+      loginPage = new LoginPage(page);
+      inventoryPage = new InventoryPage(page);
+      cartPage = new CartPage(page);
+
+      await loginPage.goto();
+      await loginPage.login("visual_user", "secret_sauce");
+
+      await inventoryPage.goto();
+      await inventoryPage.addItemToCartByName(product);
+    });
+
+    test(`Should remove ${product} from cart using the remove button`, async () => {
+      await cartPage.goto();
+
+      const cartItemsBeforeRemoval = await cartPage.getCartItems();
+      expect(cartItemsBeforeRemoval).toContain(product);
+
+      await cartPage.removeItemByName(product);
+
+      const cartItemsAfterRemoval = await cartPage.getCartItems();
+      expect(cartItemsAfterRemoval).not.toContain(product);
+
+      const itemCount = await cartPage.getCartItemCount();
+      expect(itemCount).toBe(0);
+    });
+  });
+});
